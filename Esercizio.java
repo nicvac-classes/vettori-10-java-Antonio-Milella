ef8247b8c9ef7cb094b53f11b6f96db7b4dad0e5
Antonio-Milella <antonio.milella@marconibari.edu.it>
2024-12-02 17:34:46 +0000

    ho consegnato.

--- Esercizio.java
+++ Esercizio.java
@@ -2,25 +2,45 @@
 
 //Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
+import java.lang.Math;
 
 // Classe principale, con metodo main
 class Esercizio {
     // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+    private static Random random = new Random();
+    private static Scanner in = new Scanner(System.in);
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
-
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+    public static void main(String[] args) {
+        int n, i, contatore;
+        double media;
+        
+        n = in.nextInt();
+        String[] studenti = new String[n];
+        int[] votazioni = new int[n], crediti = new int[n];
+        
+        media = 0;
+        for (i = 0; i <= n - 1; i++) {
+            studenti[i] = in.nextLine();
+            votazioni[i] = random.nextInt(11);
+            crediti[i] = random.nextInt(31);
+            media = media + votazioni[i];
+        }
+        media = media / n;
+        contatore = 0;
+        for (i = 0; i <= n - 1; i++) {
+            if (votazioni[i] > media && crediti[i] >= 6) {
+                System.out.println("Nome: " + studenti[i] + " Votazione: " + votazioni[i] + " credito formativo di: " + crediti[i]);
+                contatore = contatore + 1;
+            }
+        }
+        if (contatore == 0) {
+            System.out.println("Nessuno ha superato la quantità di 6 crediti formativi");
+        }
     }
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
+
+
+
